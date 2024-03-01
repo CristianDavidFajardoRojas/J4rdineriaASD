25768219fdf576e308c9bb950ea543f693b44cd8
CristianDavidFajardoRojas <160773269+CristianDavidFajardoRojas@users.noreply.github.com>
2024-03-01 09:23:00 -0500

    first commit

---
+++ modules/getClients.py
@@ -0,0 +1,4 @@
+from storages.cliente import clientes
+
+def searchName():
+    print(clientes)
---
+++ storages/detalle_perdido.py
@@ -0,0 +1,9 @@
+detalle_perdido = [
+    {
+        "codigo_perdido": 1,
+        "codigo_producto": "",
+        "cantidad" 0,
+        "precio_unidad": 1.0,
+        "numero_linea": 0
+    }
+]
