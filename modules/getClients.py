2984c3bb1a9836304d538ae49de9e9004c15caf2
CrisitanDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-03-04 09:07:36 -0500

    Filtros de clase

--- modules/getClients.py
+++ modules/getClients.py
@@ -1,4 +1,37 @@
-from storages.cliente import clientes
+import storages.cliente as cli
 
-def searchName():
-    print(clientes)
+def search():
+    ClienteName = list()
+    for val in cli.clientes:
+        CodName = dict({
+            "codigo_cliente": val.get("codigo_cliente"),
+            "nombre_cliente": val.get("nombre_cliente")
+        })
+        ClienteName.append(CodName)
+    return ClienteName
+
+def getOneClienteCodigo(codigo):
+    for val in cli.clientes:
+        if(val.get("codigo_cliente") == codigo):
+            return{
+                "codigo_cliente": val.get("codigo_cliente"),
+                "nombre_cliente": val.get("nombre_cliente")
+            }
+        
+def getAllClientsCreditCiudad(limiteCredit, ciudad):
+    clienteCredito = list()
+    for val in cli.clientes:
+        if(val.get("limite_credito") >= limiteCredit and val.get("ciudad") == ciudad):
+            clienteCredito.append(val)
+    return clienteCredito
+
+def getAllClientsPaisegionCiudad(Pais, Region=None, Ciudad=None):
+    ClienteZona = list()
+    for val in cli.clientes:
+        if (
+            val.get("pais") == Pais or 
+            (val.get("region") == Region or val.get("region") == None) and 
+            (val.get("ciudad") == Ciudad or val.get("ciudad") == None)
+        ):
+            ClienteZona.append(val)
+    return ClienteZona
