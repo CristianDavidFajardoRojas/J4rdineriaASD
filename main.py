0f9c46dcaac20612ab7876528969337336496ee2
CristianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-03-07 09:13:27 -0500

    Filtros clase 7 marzo: Ejercicios 9 10 11 y 12

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-#from tabulate import tabulate
+from tabulate import tabulate
 import modules.getClients as cliente
 import modules.getEmpleados as Empleados
 import modules.getPedidos as Pedidos
 import modules.getOficina as Oficina
 
-print(Pedidos.getAllEstadosPedido())
-#, tablefmt="grid"))
+print(tabulate(Pedidos.getAllPedidosEntregadosEnero(), tablefmt="rounded_grid"))
--- modules/getPedidos.py
+++ modules/getPedidos.py
@@ -1,4 +1,5 @@
 import storages.pedido as ped
+from datetime import datetime
 
 def getAllEstadosPedido():
     pedidoEstado = []
@@ -8,3 +9,65 @@ def getAllEstadosPedido():
             "estado": val.get('estado')
         })
     return pedidoEstado
+
+def getAllPedidosEntregadosAtrasadosDeTiempo():
+    pedidosEntregado = list()
+    for val in ped.pedido:
+        if (val.get("estado") == "Entregado" and val.get("fecha_entrega") == None):
+            val["fecha_entrega"] = val.get("fecha_esperada")
+        if (val.get("estado") == "Entregado"):
+            date_1 = "/".join(val.get("fecha_entrega").split("-")[::-1])
+            date_2 = "/".join(val.get("fecha_esperada").split("-")[::-1])
+            start = datetime.strptime(date_1, "%d/%m/%Y")
+            end = datetime.strptime(date_2, "%d/%m/%Y")
+            diff = end.date() - start.date()
+            if diff.days < 0:
+                pedidosEntregado.append({
+                    "codigo_de_pedido": val.get("codigo_pedido"),
+                    "codigo_de_cliente": val.get("codigo_cliente"),
+                    "fecha_esperada": val.get("fecha_esperada"),
+                    "fecha_de_entrega": val.get("fecha_entrega")
+                })
+    return pedidosEntregado
+
+#Filtro 10
+def getAllPedidosEntregadosAlmenos2DiasAntes():
+    pedidosEntregado = list()
+    for val in ped.pedido:
+        if (val.get("estado") == "Entregado" and val.get("fecha_entrega") == None):
+            val["fecha_entrega"] = val.get("fecha_esperada")
+        if (val.get("estado") == "Entregado"):
+            date_1 = "/".join(val.get("fecha_entrega").split("-")[::-1])
+            date_2 = "/".join(val.get("fecha_esperada").split("-")[::-1])
+            start = datetime.strptime(date_1, "%d/%m/%Y")
+            end = datetime.strptime(date_2, "%d/%m/%Y")
+            diff = end.date() - start.date()
+            if diff.days >= 2:
+                pedidosEntregado.append({
+                    "codigo_de_pedido": val.get("codigo_pedido"),
+                    "codigo_de_cliente": val.get("codigo_cliente"),
+                    "fecha_esperada": val.get("fecha_esperada"),
+                    "fecha_de_entrega": val.get("fecha_entrega")
+                })
+    return pedidosEntregado
+
+#Filtro 11
+def getAllPedidosRechazados2009():
+    rechazados2009 = list()
+    for val in ped.pedido:
+        FechaPedido = "/".join(val.get("fecha_pedido").split("-")[::-1])
+        start = datetime.strptime(FechaPedido, "%d/%m/%Y")
+        if val.get("estado") == "Rechazado" and start.year == 2009:
+            rechazados2009.append(val)
+    return rechazados2009
+
+#Filtro 12
+def getAllPedidosEntregadosEnero():
+    EntregadosEnero = list()
+    for val in ped.pedido:
+        if (val.get("estado") == "Entregado" and val.get("fecha_entrega") != None):
+            FechaEntregada = "/".join(val.get("fecha_entrega").split("-")[::-1])
+            start = datetime.strptime(FechaEntregada, "%d/%m/%Y")
+            if val.get("estado") == "Entregado" and start.month == str(1).zfill(2):
+                EntregadosEnero.append(val)
+    return EntregadosEnero
